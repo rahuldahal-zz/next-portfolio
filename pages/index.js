c79dc6991aa50ef8e9b5bb96241ce1923ef0412e
Rahul Dahal <rdaahal@gmail.com>
2021-01-11 15:49:54 +0545

    initial commit after customization

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      <h1>Rahul Dahal</h1>
+      <p>
+        This website us being re-developed with{" "}
+        <a href="https://nextjs.org">Next.js</a> and will be updated soon.
+      </p>
     </div>
-  )
+  );
 }
