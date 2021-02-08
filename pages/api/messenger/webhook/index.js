7b85ebce4b97dc79067ca6a4daa7b54365f2c6ad
Rahul Dahal <rdaahal@gmail.com>
2021-02-08 18:18:08 +0545

    feat: messenger webhook integration

---
+++ pages/api/messenger/webhook/index.js
@@ -0,0 +1,51 @@
+import nc from "next-connect";
+
+const app = nc();
+
+// Creates the endpoint for our webhook
+app.post((req, res) => {
+  const { body } = req;
+
+  // Checks this is an event from a page subscription
+  if (body.object === "page") {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach((entry) => {
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      const webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
+
+// Adds support for GET requests to our webhook
+app.get((req, res) => {
+  // Your verify token. Should be a random string.
+  const VERIFY_TOKEN = process.env.WEBHOOK_VERIFY_TOKEN;
+
+  // Parse the query params
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+export default app;
