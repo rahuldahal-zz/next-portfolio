db8849d3133b0f8fac7ccce291c0ee51704c3ddf
Rahul Dahal <rdaahal@gmail.com>
2021-02-09 21:19:24 +0545

    feat: fb messenger listen/respond to messages

--- pages/api/messenger/webhook/index.js
+++ pages/api/messenger/webhook/index.js
@@ -14,13 +14,25 @@ app.post((req, res) => {
       // will only ever contain one message, so we get index 0
       const webhookEvent = entry.messaging[0];
       console.log(webhookEvent);
+
+      // Get the sender PSID
+      const senderPsid = webhookEvent.sender.id;
+      console.log("Sender PSID: " + senderPsid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhookEvent.message) {
+        handleMessage(senderPsid, webhookEvent.message);
+      } else if (webhookEvent.postback) {
+        handlePostback(senderPsid, webhookEvent.postback);
+      }
     });
 
     // Returns a '200 OK' response to all requests
     res.status(200).send("EVENT_RECEIVED");
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
+    res.status(404).end();
   }
 });
 
@@ -43,9 +55,108 @@ app.get((req, res) => {
       res.status(200).send(challenge);
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+      res.status(403).end();
     }
   }
 });
+
+// Handles messages events
+function handleMessage(senderPsid, receivedMessage) {
+  let response;
+
+  // Checks if the message contains text
+  if (receivedMessage.text) {
+    // Create the payload for a basic text message, which
+    // will be added to the body of your request to the Send API
+    response = {
+      text: `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`,
+    };
+  } else if (receivedMessage.attachments) {
+    // Get the URL of the message attachment
+    const attachmentUrl = receivedMessage.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachmentUrl,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+  }
+
+  // Send the response message
+  callSendAPI(senderPsid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(senderPsid, receivedPostback) {
+  let response;
+
+  // Get the payload for the postback
+  const { payload } = receivedPostback;
+
+  // Set the response based on the postback payload
+  if (payload === "yes") {
+    response = { text: "Thanks!" };
+  } else if (payload === "no") {
+    response = { text: "Oops, try sending another image." };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(senderPsid, response);
+}
+
+// Sends response messages via the Send API
+async function callSendAPI(senderPsid, response) {
+  // The page access token we have generated in your app settings
+  const token = process.env.MESSENGER_PLATFORM_TOKEN;
+
+  // Construct the message body
+  const requestBody = {
+    recipient: {
+      id: senderPsid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+
+  const url = new URL("https://graph.facebook.com/v2.6/me/messages");
+  url.search = new URLSearchParams({ access_token: token }).toString();
+
+  try {
+    const req = await fetch(url.href, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    const { status } = req;
+    console.log("The messenger API responded with", status);
+  } catch (error) {
+    console.error("Unable to send message:", error);
+  }
+}
 
 export default app;
