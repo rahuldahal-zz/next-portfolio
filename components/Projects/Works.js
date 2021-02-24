79ec85cecff6c1594de40142e71b0fff1d201db1
Rahul Dahal <rdaahal@gmail.com>
2021-02-24 16:05:01 +0545

    feat: sub-tabs on project
    feat: UI is completed

--- components/Projects/Works.js
+++ components/Projects/Works.js
@@ -1,7 +1,9 @@
 import Button from "@components/Common/Button/Button";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Works({ works }) {
+  const [activeWork, setActiveWork] = useState({ work: "", tab: "overview" });
+
   return (
     <main className="projects">
       <div className="contentsWrap">
@@ -9,6 +11,62 @@ export default function Works({ works }) {
       </div>
     </main>
   );
+
+  function Content({ data }) {
+    return (
+      <p
+        dangerouslySetInnerHTML={{
+          __html: `${data}`,
+        }}
+      />
+    );
+  }
+
+  function getAppropriateProjectContent(
+    workName,
+    { overview, features, learnings }
+  ) {
+    const { work, tab } = activeWork;
+
+    if (!work || work !== workName) {
+      return <Content data={overview} />;
+    }
+    if (tab === "features") {
+      return <Content data={features} />;
+    }
+    if (tab === "learnings") {
+      return <Content data={learnings} />;
+    }
+    return <Content data={overview} />;
+  }
+
+  function getTabClassName(tabName, workName) {
+    const { work, tab } = activeWork;
+    if (tabName === tab && workName === work) {
+      return "project__descriptionTab project__descriptionTab--active";
+    }
+    if (workName !== work && tabName === "overview") {
+      return "project__descriptionTab project__descriptionTab--active";
+    }
+    return "project__descriptionTab";
+  }
+
+  function DescriptionTabs({ work }) {
+    const tabs = ["Overview", "Features", "Learnings"];
+    return (
+      <div className="project__descriptionTabs">
+        {tabs.map((tab) => (
+          <button
+            type="button"
+            className={getTabClassName(tab.toLowerCase(), work)}
+            onClick={() => setActiveWork({ work, tab: tab.toLowerCase() })}
+          >
+            {tab}
+          </button>
+        ))}
+      </div>
+    );
+  }
 
   function Projects({ works }) {
     return (
@@ -25,28 +83,36 @@ export default function Works({ works }) {
           } = work.data;
           return (
             <div className="project" key={name.trim().split(" ").join("")}>
-              <h4 className="project__name">{name}</h4>
+              <div className="project__info">
+                <h4 className="project__name">{name}</h4>
+                <span className="project__stack">{stack}</span>
+              </div>
               <div className="project__body">
-                <div className="project__description">
-                  <p
-                    className="description"
-                    dangerouslySetInnerHTML={{
-                      __html: `${name} is ${description}`,
-                    }}
-                  />
-                  <span className="project__stack">{stack}</span>
-                  <Button
-                    fill="filled"
-                    textContent="Visit"
-                    to={url}
-                    modifier="project__url"
-                  />
-                  <Button
-                    fill="outline"
-                    textContent="Github"
-                    to={repo}
-                    modifier="project__repo"
-                  />
+                <div className="project__details">
+                  <div className="project__description">
+                    <div className="project__descriptionContent">
+                      {getAppropriateProjectContent(name, {
+                        overview: description,
+                        features: "Features Placeholder",
+                        learnings: "Learnings Placeholder",
+                      })}
+                    </div>
+                    <DescriptionTabs work={name} />
+                  </div>
+                  <div className="project__actions">
+                    <Button
+                      fill="filled"
+                      textContent="Visit"
+                      to={url}
+                      modifier="project__url"
+                    />
+                    <Button
+                      fill="outline"
+                      textContent="Github"
+                      to={repo}
+                      modifier="project__repo"
+                    />
+                  </div>
                 </div>
 
                 <div className="project__screenshots">
