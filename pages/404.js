94cc6b780def1ec28b61d63214a90af39cf61ee2
Rahul Dahal <rdaahal@gmail.com>
2021-06-08 15:28:02 +0545

    feat: add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "@components/Common/Link/Link";
+
+export default function Custom404() {
+  return (
+    <main className="custom404">
+      <h1>
+        Error{" "}
+        <Link
+          to="https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404"
+          textContent="404"
+        />
+      </h1>
+      <p>The page you are requesting for is not found!</p>
+      <Link to="/" textContent="Go Back Home" fill="filled" />
+    </main>
+  );
+}
