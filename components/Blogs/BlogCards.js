5e098a68815f62de3b0c08dd8bd18338dd04fc8e
Rahul Dahal <rdaahal@gmail.com>
2021-06-10 13:42:10 +0545

    fix:style UI for published blogs

--- components/Blogs/BlogCards.js
+++ components/Blogs/BlogCards.js
@@ -8,20 +8,21 @@ export default function BlogCards({ cardDetails }) {
   }
 
   return (
-    <section className="blogs__content">
-      {cardDetails.map((article) => {
+    <section className="blogCards__content">
+      {cardDetails.map((article, index) => {
         const { _id, title, brief, dateAdded, coverImage, slug, tags } =
           article;
         return (
-          <Link
-            to={getLink(slug)}
-            className="blogCard"
-            key={_id}
-            onClick={() => showLoader()}
-          >
+          <div className="blogCard" key={index}>
             <img src={coverImage} alt={title} className="blogCard__cover" />
             <div className="blogCard__content">
-              <h4 className="blogCard__title">{title}</h4>
+              <h4 className="blogCard__title">
+                <Link
+                  to={getLink(slug)}
+                  textContent={title}
+                  onClick={() => showLoader()}
+                />
+              </h4>
               <p className="blogCard__tags">
                 <em>#javascript</em>
               </p>
@@ -29,8 +30,15 @@ export default function BlogCards({ cardDetails }) {
                 {new Date(dateAdded).toLocaleString()}
               </p>
               <p className="blogCard__brief">{brief}</p>
+              <Link
+                to={getLink(slug)}
+                textContent="Read more"
+                fill="filled"
+                className="blogCard__link"
+                onClick={() => showLoader()}
+              />
             </div>
-          </Link>
+          </div>
         );
       })}
     </section>
