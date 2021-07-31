ad41fb313c15c3671575b98f0161cc6c77a139fa
Rahul Dahal <rdaahal@gmail.com>
2021-07-31 13:50:44 +0545

    fix: add classssName prop

--- components/Picture/index.js
+++ components/Picture/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-export default function Picture({ source, alt, width, height }) {
+export default function Picture({
+  source,
+  alt,
+  width,
+  height,
+  className = "",
+}) {
   const { defaultSrc } = source;
   delete source.default;
   const srcs = Object.keys(source);
 
   return (
-    <picture>
+    <picture className={className}>
       {srcs.map((src, index) => (
         <source key={index} srcSet={source[src]} type={`image/${src}`} />
       ))}
